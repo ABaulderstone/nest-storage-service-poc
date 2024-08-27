1356bde7c10888616d583c9425cacd3ffdc9c954
ABaulderstone <alex.baulderstone@gmail.com>
2024-08-27 14:59:02 +1000

    adds s3 service

--- nest-app/src/storage/s3-storage.service.ts
+++ nest-app/src/storage/s3-storage.service.ts
@@ -1,10 +1,40 @@
+import { ConfigService } from '@nestjs/config';
 import { StorageService } from './storage-service.interface';
-
+import {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 export class S3StorageService implements StorageService {
-  getPresignedUrl(key: string): Promise<string> {
-    throw new Error('Method not implemented.');
+  private s3Client: S3Client;
+  private bucket: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.s3Client = new S3Client({
+      region: this.configService.get<string>('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+    this.bucket = this.configService.get('AWS_BUCKET');
+  }
+  async getPresignedUrl(key: string): Promise<string> {
+    const command = new GetObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+    });
+    return await getSignedUrl(this.s3Client, command, { expiresIn: 3600 });
   }
   async upload(file: Express.Multer.File) {
-    return { key: 'd' };
+    const key = `${Date.now()}-${file.originalname}`;
+    const command = new PutObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+      Body: file.buffer,
+    });
+    await this.s3Client.send(command);
+    return { key };
   }
 }
